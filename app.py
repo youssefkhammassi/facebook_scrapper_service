2710a6052b868026e83d2132d01ff2993d113a1f
Youssef Khammassi <extern.youssef.khammassi@allianz.be>
2022-05-28 20:00:38 +0100

    + add GCP config

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ import os
 
 import uvicorn
 from fastapi import FastAPI
+
 app = FastAPI()
 
-#root route
+
+# root route
 @app.get("/")
 async def root():
     return {"message": "Hello"}
 
-#Heath check route
+
+# Heath check route
 @app.get("/PING")
 async def health_check():
     return {"message": "PONG"}
 
+
 def run():
-    return uvicorn.run("app:app", host='0.0.0.0', port=os.getenv('PORT', 8088))
+    return uvicorn.run("app:app", host='0.0.0.0', port=int(os.getenv('PORT', '8088')))
 
 
 if __name__ == "__main__":
